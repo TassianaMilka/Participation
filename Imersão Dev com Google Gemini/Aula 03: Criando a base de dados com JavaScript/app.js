e9f574bcbd2ccb1353eef289e337c5ad873b9114
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-09-07 09:47:55 -0300

    1

---
+++ Imersão Dev com Google Gemini/Aula 03: Criando a base de dados com JavaScript/app.js
@@ -0,0 +1,11 @@
+
+console.log(dados);
+/**console.log(dados[2]);*/
+
+/*console.log(dados[0].descricao);*/
+
+
+
+
+
+
---
+++ Imersão Dev com Google Gemini/Aula 03: Criando a base de dados com JavaScript/dados.js
@@ -0,0 +1,19 @@
+
+/*let dados = ["André Rocha", "Beatriz Souza"];*/
+
+let dados = [{
+
+    titulo: "André Rocha",
+    descricao: "André Rocha é um paraatleta brasileiro que inspira com sua história de superação e dedicação ao esporte.Após um acidente que o deixou tetraplégico, ele encontrou no lançamento de disco e arremesso de peso uma forma de seguir em frente e alcançar seus objetivos. Com diversas medalhas em competições nacionais e internacionais, incluindo ouro em Mundiais e bronze nas Paralimpíadas de Paris 2024, André Rocha é um exemplo de que com determinação e força   de vontade é possível conquistar grandes feitos, mesmo diante das maiores adversidades."              
+    
+},
+
+{
+    
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza, a Bia, marcou história ao conquistar a primeira medalha de ouro para o Brasil nos Jogos Olímpicos de Paris 2024. Com apenas 26 anos, a judoca demonstrou uma técnica impecável e uma força de vontade inabalável, superando adversárias experientes e consagrando- se como uma das maiores promessas do esporte brasileiro.Sua vitória não apenas trouxe alegria para o país, mas também inspirou uma nova geração de atletas, mostrando que com dedicação e talento é possível alcançar o topo do pódio olímpico."
+ 
+
+}
+
+];
